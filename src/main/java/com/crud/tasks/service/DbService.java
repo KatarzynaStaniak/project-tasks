b248efeadc0b0d16360fb2b7a6057fc7c5de2b13
KatarzynaStaniak <kstaniak11@gmail.com>
2019-04-15 15:06:44 +0200

    Project Tasks - getById

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -15,4 +15,8 @@ public class DbService {
     public List<Task> getAllTasks(){
         return repository.findAll();
     }
+
+    public Task getTaskById(Long taskId){
+        return repository.findTaskById(taskId);
+    }
 }
